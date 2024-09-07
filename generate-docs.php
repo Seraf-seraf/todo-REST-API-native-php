8d224e8d294b25170513c2c147cbf11e1811f0cf
Seraf-seraf <serafim123sima@mail.ru>
2024-09-07 13:56:57 +0600

    add openapi.yaml

--- generate-docs.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("./vendor/autoload.php");
-
-$openapi = \OpenApi\Generator::scan(['./app']);
-
-header('Content-Type: application/x-yaml');
-$yml = $openapi->toYaml();
-
-file_put_contents('docs/openapi.yml', $yml);
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,14 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'ToDo\\' => array($baseDir . '/'),
+    'ToDo\\' => array($baseDir . '/app'),
+    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
+    'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
+    'Symfony\\Component\\Finder\\' => array($vendorDir . '/symfony/finder'),
+    'Psr\\Log\\' => array($vendorDir . '/psr/log/src'),
+    'Psr\\Cache\\' => array($vendorDir . '/psr/cache/src'),
+    'OpenApi\\' => array($vendorDir . '/zircote/swagger-php/src'),
+    'Doctrine\\Deprecations\\' => array($vendorDir . '/doctrine/deprecations/lib/Doctrine/Deprecations'),
+    'Doctrine\\Common\\Lexer\\' => array($vendorDir . '/doctrine/lexer/src'),
+    'Doctrine\\Common\\Annotations\\' => array($vendorDir . '/doctrine/annotations/lib/Doctrine/Common/Annotations'),
 );
